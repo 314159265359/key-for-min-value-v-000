0f735af4628926967ee342b46c976cbca9d773c1
Karen G <kgraves0701@gmail.com>
2018-03-18 02:58:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,13 @@
 def key_for_min_value(hash)
   return nil if hash == {}
   #holder = hash.values.max
-  hold = hash.max_by{|k,v| v}
-  holder = hold[1]
+  #hold = hash.max_by{|k,v| v}
+  #holder = hold[1]
+  first_time = true
   hold_name = ""
   hash.each do |name, value|
+     holder = value if first_time
+     first_time = false
      if value < holder
      hold_name = name
      holder = value
